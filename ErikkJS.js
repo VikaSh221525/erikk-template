f07e5ca0bba3079616d32e57d28fd119be639ada
VikaSh221525 <vikassharma221525@gmail.com>
2025-05-12 02:30:51 +0530

    scroll effect

--- ErikkJS.js
+++ ErikkJS.js
@@ -8,3 +8,51 @@ document.addEventListener("mousemove", function(e){
     });
 
 })
+gsap.from("#fashionwerk",{
+    y: 50,        // start 100px below
+    opacity: 0,    // start fully transparent
+    duration: 2, // animation duration
+    ease: "power4.out", // smooth easing
+    delay: 0.2     // slight delay after page load
+})
+
+gsap.from("#ourservice",{
+    y: 50,        // start 100px below
+    opacity: 0,    // start fully transparent
+    duration: 2, // animation duration
+    ease: "power4.out", // smooth easing
+    delay: 0.2,     // slight delay after page load
+    scrollTrigger:{
+        trigger:"#ourservice",
+        scroller:"body",
+        start: "top 80%",
+        // markers:true
+    }
+})
+
+gsap.from("#selectionwork",{
+    y: 50,        // start 100px below
+    opacity: 0,    // start fully transparent
+    duration: 2, // animation duration
+    ease: "power4.out", // smooth easing
+    delay: 0.2,     // slight delay after page load
+    scrollTrigger:{
+        trigger:"#selectionwork",
+        scroller:"body",
+        start: "top 80%",
+        // markers:true
+    }
+})
+gsap.from("#clientimage",{
+    y: 50,        // start 100px below
+    opacity: 0,    // start fully transparent
+    duration: 2, // animation duration
+    ease: "power4.out", // smooth easing
+    delay: 0.2,     // slight delay after page load
+    scrollTrigger:{
+        trigger:"#clientimage",
+        scroller:"body",
+        start: "top 80%",
+        // markers:true
+    }
+})
